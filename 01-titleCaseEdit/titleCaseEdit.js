a70f481a7bf8a6d55602b106e5e37fd11a54d096
Vincent Ndokaj <vinndokaj@gmail.com>
2020-12-28 18:53:12 -0500

    01-titleCaseEdit complete

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,18 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  all_words = title.split(" ");
+  for(var i = 0; i < all_words.length; i++){
+    all_words[i] = capitalizeFirstLetter(all_words[i]);
+  }
+  newTitle = all_words.join(" ");
+  return newTitle;
+}
+
+function capitalizeFirstLetter(word) {
+  firstLetter = word.substr(0,1).toUpperCase();
+  restOfWord = word.substr(1);
+  str = firstLetter + restOfWord;
+  return str;
 }
 
 // Do not edit this line;
