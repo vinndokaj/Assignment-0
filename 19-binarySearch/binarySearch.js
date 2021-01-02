1ba831d8d91910fe4f2411acd775475bf94525aa
Vincent Ndokaj <vinndokaj@gmail.com>
2021-01-02 10:30:09 -0500

    19 complete

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,17 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    var i = Math.floor(nums.length/2);
+    var middle = nums[i];
+    if(middle == target){
+      return true;
+    } else if (nums.length == 1){
+      return false;
+    } else if (middle < target){
+      return this.binarySearch(nums.slice(i), target);
+    } else if (middle > target){
+      return this.binarySearch(nums.slice(0, i), target);
+    }
   }
 }
 
